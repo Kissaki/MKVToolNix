acccad3afcc1f54e8d2bcdf192538ba607b07fc8
Jan Klass <kissaki@posteo.de>
2023-06-09 16:51:30 +0200

    misc(ci): Create all releases

--- update-mirror.sh
+++ update-mirror.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Comparing tags…"
-dups=.git/refs/upstream/tags/
-dmir=.git/refs/mirror/tags/
-for fpath in ${dups}*
-do
-  tag=${fpath#${dups}}
-  if [ ! -e "${dmir}${tag}" ]
-  then
-    echo "Identified new tag ${tag}"
-
-    # Push tag to mirror
-    echo "Pushing tag to mirror…"
-    git push origin refs/upstream/tags/${tag}:refs/tags/${tag}
-
-    echo "Extracting changelog…"
-    rnfile="rn.md"
-    ./extract-release-notes.sh "${tag}" "${rnfile}"
-
-    echo "Creating new release…"
-    echo "hub release create --file \"${rnfile}\" \"$tag\""
-    hub release create --file "${rnfile}" "$tag"
-  fi
-done
+rnfile="rn.md"
+tag="release-76.0"
+./extract-release-notes.sh "${tag}" "${rnfile}"
+hub release create --file "${rnfile}" "$tag"
+tag="release-75.0.0"
+./extract-release-notes.sh "${tag}" "${rnfile}"
+hub release create --file "${rnfile}" "$tag"
+tag="release-74.0.0"
+./extract-release-notes.sh "${tag}" "${rnfile}"
+hub release create --file "${rnfile}" "$tag"
+tag="release-73.0.0"
+./extract-release-notes.sh "${tag}" "${rnfile}"
+hub release create --file "${rnfile}" "$tag"
+tag="release-72.0.0"
+./extract-release-notes.sh "${tag}" "${rnfile}"
+hub release create --file "${rnfile}" "$tag"
+tag="release-71.1.0"
+./extract-release-notes.sh "${tag}" "${rnfile}"
+hub release create --file "${rnfile}" "$tag"
+tag="release-71.0.0"
+./extract-release-notes.sh "${tag}" "${rnfile}"
+hub release create --file "${rnfile}" "$tag"
