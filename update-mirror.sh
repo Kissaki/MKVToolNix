888ce35ffe4baabf88774d0638be5397ae1ea05c
Jan Klass <kissaki@posteo.de>
2026-05-25 19:32:02 +0200

    Switch to show-ref and rev-parse

--- update-mirror.sh
+++ update-mirror.sh
@@ -2,23 +2,27 @@
 set -e
 
 echo "Comparing tags…"
-dups=.git/refs/upstream/tags/
-dmir=.git/refs/mirror/tags/
-for fpath in ${dups}*; do
-  tag=${fpath#${dups}}
-  if [ ! -e "${dmir}${tag}" ]; then
-    echo "Identified new tag ${tag}"
+dups=refs/upstream/tags/
+dmir=refs/mirror/tags/
+git show-ref --tags | while read -r commit ref; do
+  if [[ "$ref" == "$dups"* ]]; then
+    tag="${ref#$dups}"
+    mirror_ref="${dmir}${tag}"
+    git rev-parse --verify --quiet ${mirror_ref}
+    if ! git rev-parse --verify --quiet "$mirror_ref"; then
+      echo "Identified new tag ${tag}"
+      
+      # Push tag to mirror
+      echo "Pushing tag to mirror…"
+      git push origin refs/upstream/tags/${tag}:refs/tags/${tag}
 
-    # Push tag to mirror
-    echo "Pushing tag to mirror…"
-    git push origin refs/upstream/tags/${tag}:refs/tags/${tag}
+      echo "Extracting changelog…"
+      rnfile="rn.md"
+      ./extract-release-notes.sh "${tag}" "${rnfile}"
 
-    echo "Extracting changelog…"
-    rnfile="rn.md"
-    ./extract-release-notes.sh "${tag}" "${rnfile}"
-
-    echo "Creating new release…"
-    echo "gh release create \"$tag\" --notes-file \"${rnfile}\""
-    gh release create "$tag" --notes-file "${rnfile}" 
+      echo "Creating new release…"
+      echo "gh release create \"$tag\" --notes-file \"${rnfile}\""
+      gh release create "$tag" --notes-file "${rnfile}" 
+    fi
   fi
 done
